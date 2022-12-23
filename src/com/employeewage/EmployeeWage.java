d33449b86af9784c971555ac16563bc2fb90e841
PrathmPatil <prathmpatil2818@gmail.com>
2022-12-23 22:56:02 +0530

    Random Number by Method

--- src/com/employeewage/EmployeeWage.java
+++ src/com/employeewage/EmployeeWage.java
@@ -1,8 +1,16 @@
 package com.employeewage;
 
-public class EmployeeWage {
+import java.util.Random;
 
+public class EmployeeWage {
+  int rNumber;
 	 public static void print() {
 		 System.out.println("***************Welcome to Employee Wage Computation***************");
+	 }
+	 public static void  rNum() {
+		 EmployeeWage employeewage=new EmployeeWage();
+		 Random random= new Random();
+		 employeewage.rNumber=random.nextInt(2);
+		 System.out.println("Random Number is ::- "+employeewage.rNumber);
 	 }
 }
--- src/com/employeewage/Main.java
+++ src/com/employeewage/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		EmployeeWage employeewage =new EmployeeWage();
-		employeewage.print();;
+		employeewage.print();
+		employeewage.rNum();
 	}
 
 }
